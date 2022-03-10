0cf746c685608c0334a7385ee1106ae81561b218
emily <emilyvicki601@gmail.com>
2022-03-10 13:59:54 +0800

    add: check credit card valid or not

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
+# Validates credit card number using Luhn Algorithm
 module LuhnValidator
-  # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
@@ -7,5 +9,16 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    sum = nums_a[0...-1]
+          .reverse
+          .each_with_index
+          .reduce(0) do |start, (digit, index)|
+            start + cal_digit(digit, index)
+          end
+    nums_a[-1] == (10 - sum % 10) % 10
+  end
+
+  def cal_digit(digit, index)
+    (index.even? ? digit * 2 : digit).digits.reduce { |d1, d2| d1 + d2 }
   end
 end
